784188441b0caeebab5cbdf0eb1a0e6b9dd89e53
k2s09 <kavinsood15@gmail.com>
2024-05-26 15:45:46 +0530

    serverjs files

--- server.js
+++ server.js
@@ -21,11 +21,20 @@ app.use(session({
 app.use(express.static(__dirname));
 
 // MySQL database connection
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'rcvwzS4KJ',
-    database: 'timesheet'
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+    return;
+  }
+  console.log('Connected to the MySQL database');
 });
 
 app.get('/getTimesheet', (req, res) => {
